56be9a8e42a4f83cadb99a825557cdda0da51470
Michael Smith <mas9311@gmail.com>
2020-07-26 23:48:24 -0600

    Added basic photosensitivity warning for Windows OS

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from src import *
+from sys import platform
 
 
 if __name__ == '__main__':
@@ -9,8 +10,21 @@ if __name__ == '__main__':
         'verbose': 0,
         'demo': False
     }
-    # running 2 instances of this program? Terminate first process with SIGTERM.
-    kill_all_running_instances(__file__, arg_dict['verbose'])
+
+    if platform in ['win32', 'cygwin']:
+        # OS is Windows
+        input_str = (f'\nPHOTOSENSITIVE WARNING:\n'
+                     f'  Windows OS has not been thoroughly tested or optimized.\n'
+                     f'  The screen will be redrawn in a crude manner.\n\n'
+                     f'**May cause epileptic seizures.**\n\n'
+                     f'Are you sure you wish to continue?\n'
+                     f' - Enter `y` to continue. Enter anything else to quit.\n'
+                     f' > ')
+        if input(input_str).lower() != 'y':
+            sys.exit()
+    else:
+        # running 2 instances of this program? Terminate first process with SIGTERM.
+        kill_all_running_instances(__file__, arg_dict['verbose'])
 
     correct_cwd()  # cd /.../shade*/
 
