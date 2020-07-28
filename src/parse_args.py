dfb2c80df3f3397dbedcb882388610d197738404
Michael Smith <mas9311@gmail.com>
2020-07-28 03:22:14 -0600

    Print a blinking red Error message when given an unknown arg. Cleaned up superficial spacing in the output of the --help message. py_cmd only invokes platform.system() once.

--- src/parse_args.py
+++ src/parse_args.py
@@ -7,9 +7,16 @@ from src.defaults import DEFAULT_ARGUMENTS
 def retrieve_parameters():
     """Retrieves the parameters from the console if provided.
     Returns the parameters in dict format.
-    If an unknown argument is passed, print the --help screen.
-    If no arguments are passed, then print the intro Welcome screen."""
+    If an unknown argument is passed, print the --help screen and halt execution."""
+    default = DEFAULT_ARGUMENTS
 
+    usage_str = py_cmd('[options]') + ' ' * 39 + '┃\n\n'
+    cmd_description = (
+        '         ╔═════════════════════════════════════════════════════╗         ┃\n'
+        '         ║ Monitor overlay to lighten or darken monitor output ║         ┃\n'
+        '         ╚═════════════════════════════════════════════════════╝         ┃\n'
+        '                                                                         ┃\n'
+        '━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛')
     version_description = (
         '        ☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐           \n'
         '        ☐              ☐  screen-shade  v0.0.0  ☐              ☐           \n'
@@ -17,54 +24,51 @@ def retrieve_parameters():
         '        ☐                                                      ☐           \n'
         '        ☐   Check if there are any new releases for this at:   ☐           \n'
         '        ☐   https://github.com/Mas9311/screen-shade/releases   ☐           \n'
-        '        ☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐           '
-    )
-
-    default = DEFAULT_ARGUMENTS
-
-    cmd_description = (
-        '⠀⠀⠀⠀⠀⠀⠀╔═════════════════════════════════════════════════════╗⠀⠀⠀⠀⠀⠀⠀┃\n'
-        '⠀⠀⠀⠀⠀⠀⠀║ Monitor overlay to lighten or darken monitor output ║⠀⠀⠀⠀⠀⠀⠀┃\n'
-        '⠀⠀⠀⠀⠀⠀⠀╚═════════════════════════════════════════════════════╝⠀⠀⠀⠀⠀⠀⠀┃\n'
-        '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀⠀⠀⠀⠀⠀┃\n'
-        '━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛'
-    )
+        '        ☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐           ')
 
     # noinspection PyTypeChecker
     parser = ArgumentParser(add_help=False,
                             description=cmd_description,
                             formatter_class=RawTextHelpFormatter,
-                            usage=py_cmd('[options]') + (' ' * 39) + '┃')
+                            usage=usage_str)
 
     parser.add_argument('-d', '--demo',
                         action='store_true',
                         default=default['demo'],
                         dest='demo',
-                        help='Demo:    default = %(default)s' + ' ' * 38)
+                        help='DEMO: visualizes how the geometry is rendered.' + ' ' * 13 + '┃\n'
+                             '  default = %(default)s' + ' ' * 42 + '┃')
 
     parser.add_argument('-v',
                         action='count',
                         default=default['verbose'],
                         dest='verbose',
-                        help='Verbose: default = %(default)s.' + ' ' * 41 + '\n'
-                             ' -v   Basic logger\n'
-                             ' -vv  Advanced debugger\n'
-                             ' -vvv Developer debugger')
+                        help='VERBOSE: logs actions to std.out stream.' + ' ' * 19 + '┃\n'
+                             '  default = %(default)s' + ' ' * 46 + '┃\n'
+                             '  -v   Basic logger' + ' ' * 40 + '┃\n'
+                             '  -vv  Advanced debugger' + ' ' * 35 + '┃\n'
+                             '  -vvv Developer debugger' + ' ' * 34 + '┃')
 
     parser.add_argument('--version',
                         action='version',
                         version=version_description,
-                        help='VERSION prints to console, and exits')
+                        help='VERSION prints to console, and exits.' + ' ' * 22 + '┃')
 
     parser.add_argument('-h', '--help',
                         action='help',
                         default=SUPPRESS,
-                        help='HELP message (this) is displayed, and exits')
+                        help='HELP message (this) is displayed, and exits.' + ' ' * 15 + '┃')
 
     known_args, unknown_args = parser.parse_known_args()
 
     if unknown_args:  # user added unknown arguments
-        print(f'Unknown arguments detected: {str(unknown_args)}\n > calling --help screen\n')
+        # TODO: make an enum for colors: "from enum import Enum; class Color(Enum): ..."
+        blink = '\x1b[6m'  # blinks every second
+        red = '\x1b[1;31m'  # light red color
+        clear = '\x1b[0m'  # clear all formatting (color and font)
+        print(f'{blink}{red}*** Fatal Error ***{clear}\n'
+              f' > Unknown arguments detected: {str(unknown_args)}\n'
+              f' > invoking --help\n')
         parser.print_help()  # prints help message
         time.sleep(0.25)  # pause execution just long enough for help to display
         parser.parse_args()  # prints error message and halts the execution
@@ -80,10 +84,11 @@ def retrieve_parameters():
 
 
 def py_cmd(additional=''):
-    cmd = ('python3', 'python.exe')[platform.system() == 'Windows'] + ' run.py'
+    windows_os = platform.system() == 'Windows'
+    cmd = ('python3', 'python.exe')[windows_os] + ' run.py'
     if additional:
         additional.strip()
-        num_spaces = 0 if platform.system() == 'Windows' else 3
+        num_spaces = (3, 0)[windows_os]
         spaces = ' ' * num_spaces
         cmd += ' ' + additional + spaces
     return cmd
